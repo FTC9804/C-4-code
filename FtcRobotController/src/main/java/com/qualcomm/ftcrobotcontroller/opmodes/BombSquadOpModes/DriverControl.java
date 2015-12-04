1b5f6bd7aefb2343df42d99c141870bf0d5b6f86
etiennelunetta <etiennelunetta@lhsla.org>
2015-12-03 21:42:58 -0800

    Driver Control Changed

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BombSquadOpModes/DriverControl.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BombSquadOpModes/DriverControl.java
@@ -167,6 +167,7 @@ public class DriverControl extends OpMode{
                 LeftFlipper.setPosition(flipperNotMoving);
 
             }
+
             LeftFlipper.setPosition(flipperOutPositionLeft);
         }
     }
